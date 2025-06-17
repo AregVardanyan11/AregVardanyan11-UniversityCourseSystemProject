195066b877c03f0dfba66012f129107c31602bb6
Areg <aregv11@gmail.com>
2025-06-17 14:08:46 +0400

    0.0 - Starting project, added dependencies

---
+++ src/main/java/org/example/universitysystemproject/UniversitySystemProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.universitysystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitySystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/universitysystemproject/UniversitySystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.universitysystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
